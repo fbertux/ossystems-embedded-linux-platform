21eb780089e87bfdebdbd3c24202d71519d8c326
Fabio Berton <fabio.berton@ossystems.com.br>
2020-09-16 09:55:47 -0300

    Set platform for a general use
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

---
+++ setup-environment.d/ossystems-yocto.py
@@ -0,0 +1,22 @@
+def __after_init_ossystems_yocto():
+    PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+
+    append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+                    [
+                        'meta-arm/meta-arm',
+                        'meta-freescale',
+                        'meta-freescale-3rdparty',
+                        'meta-openembedded/meta-networking',
+                        'meta-openembedded/meta-oe',
+                        'meta-openembedded/meta-python',
+                        'meta-qt5',
+                        'meta-raspberrypi',
+                        'meta-shellhub',
+                        'meta-ti',
+                        'meta-webkit',
+                    ]])
+
+    # FSL EULA
+    eulas.accept['meta-freescale/EULA'] = 'ACCEPT_FSL_EULA = "1"'
+
+run_after_init(__after_init_ossystems_yocto)
